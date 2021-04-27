950ab3a75b3b700698d11ac13dc71abb3efe1d0d
CGalko <cash.galko12@ncf.edu>
2021-04-27 19:44:53 -0400

    Added a bit more organization to the file structure. Got the schema working and db created.

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employee_manager;
+CREATE DATABASE employee_manager;
+
+
+USE employee_manager;
+
+CREATE TABLE department (
+   id INT NOT NULL AUTO_INCREMENT,
+   name VARCHAR (40),
+   primary key (id)
+);
+
+CREATE TABLE role (
+   id INT NOT NULL AUTO_INCREMENT,
+   title VARCHAR (40),
+   salary DECIMAL (8,2),
+   department_id INT,
+   foreign key (department_id) references department(id),
+   primary key (id)
+);
+
+CREATE TABLE employee (
+   id INT NOT NULL AUTO_INCREMENT,
+   first_name VARCHAR (40),
+   last_name VARCHAR (40),
+   role_id INT,
+   foreign key (role_id) references role(id),
+   manager_id INT default 0,
+   primary key (id)
+);
