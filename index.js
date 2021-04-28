dbb55eb56b954ce1cefa6941906e25d94f2cbdf1
CGalko <cash.galko12@ncf.edu>
2021-04-27 20:06:32 -0400

    Added the connection configuration to allow acces to the db. Started the inquire logic tree in index.js that will reference the actual logic definied in logic.js. Everything currently functioning.

---
+++ index.js
@@ -0,0 +1,46 @@
+const logic = require('./logic');
+const inquirer = require("inquirer");
+
+// The starting inquirer prompt that calls all needed functions to run. 
+const employee = () => {
+    inquirer
+    .prompt(
+        {
+            type: 'list',
+            message: 'Welcome to the Employee Tracker System. Please select an option.',
+            name: 'options',
+            choices: ['View Roles', 'View Departments','View Employees', 'Add Role', 'Add Department', 'Add Employee', 'Delete Employee', 'Exit'],
+        },
+    )
+    .then (response => {
+        // Calls the correct function based on the user's input from the initial menu. 
+        switch (response.options) {
+            
+            case "View Roles": viewRoles();
+            break;
+            case "View Departments": viewDepartments();
+            break;
+            case "View Employees": viewEmployees();
+            break;
+            case "Add Role": addRole();
+            break;
+            case "Add Department": addDepartment();
+            break;
+            case "Add Employee": addEmpolyee();
+            break;
+            case "Delete Employee": deleteEmployee();
+            break;
+            case "Exit": finish();
+            break;
+        }
+    });
+};
+
+const finish = () => {
+    console.log('Thank you for using the Employee Tracker System.');
+    process.exit();
+}
+
+
+// begins the initial program.
+employee();
